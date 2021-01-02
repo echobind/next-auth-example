6805c85da9ec3400aca5777dcbf9919c190a9276
Mike Cavaliere <mcavaliere@echobind.com>
2021-01-02 17:25:03 -0500

    feat: add chakra-ui

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,14 @@
 import '../styles/globals.css';
+import { ChakraProvider } from '@chakra-ui/react';
 import { Provider } from 'next-auth/client';
 
 function MyApp({ Component, pageProps }) {
   return (
     <Provider session={pageProps.session}>
       <Component {...pageProps} />
+      <ChakraProvider>
+          <Component {...pageProps} />
+      </ChakraProvider>
     </Provider>
   );
 }
