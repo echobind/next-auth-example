8f443cdaa5c0b59e3e92f7c0b80973e541534166
Mike Cavaliere <mcavaliere@echobind.com>
2021-01-02 16:30:55 -0500

    feat: adds google and github auth

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,21 @@
+import NextAuth from 'next-auth';
+import Providers from 'next-auth/providers';
+
+const options = {
+  // Configure one or more authentication providers
+  providers: [
+    Providers.GitHub({
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    }),
+    Providers.Google({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+  ],
+
+  // A database is optional, but required to persist accounts in a database
+  // database: process.env.DATABASE_URL,
+};
+
+export default (req, res) => NextAuth(req, res, options);
