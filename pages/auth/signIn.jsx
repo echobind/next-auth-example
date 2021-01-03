8fda7a6b7f0309c83a0040c342d76ec64d44d565
Mike Cavaliere <mcavaliere@echobind.com>
2021-01-03 11:54:07 -0500

    feat: adds button styling

---
+++ pages/auth/signIn.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { providers, signIn } from 'next-auth/client';
+import { Button, Flex, Icon } from '@chakra-ui/react';
+import { FcGoogle } from 'react-icons/fc';
+import { FaGithub } from 'react-icons/fa';
+
+const icons = {
+  github: <FaGithub />,
+  google: <FcGoogle />,
+};
+
+export default function SignIn({ providers }) {
+  return (
+    <Flex align='center' justify='center' direction='column' minHeight='80vh'>
+      {Object.values(providers).map((provider) => (
+        <div key={provider.name}>
+          <Button
+            size='lg'
+            onClick={() => signIn(provider.id)}
+            mb={3}
+            leftIcon={icons[provider.id]}
+          >
+            Sign in with {provider.name}
+          </Button>
+        </div>
+      ))}
+    </Flex>
+  );
+}
+
+SignIn.getInitialProps = async (context) => {
+  return {
+    providers: await providers(context),
+  };
+};
