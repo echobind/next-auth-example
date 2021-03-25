340858b415facf02dae917735e621b7c9446f923
Mike Cavaliere <mcavaliere@echobind.com>
2021-03-25 16:44:04 -0400

    feat: updates next-auth

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -14,8 +14,13 @@ const options = {
     }),
   ],
 
+  // The secret should be set to a reasonably long random string.
+  // It is used to sign cookies and to sign and encrypt JSON Web Tokens, unless
+  // a separate secret is defined explicitly for encrypting the JWT.
+  secret: process.env.SECRET,
+
   // A database is optional, but required to persist accounts in a database
   // database: process.env.DATABASE_URL,
 };
 
-export default (req, res) => NextAuth(req, res, options);
+export default NextAuth(options);
